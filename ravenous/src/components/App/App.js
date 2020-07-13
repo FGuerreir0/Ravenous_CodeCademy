8d6ad6ae6fa5910b24908bd28bcc660011f7738d
Fabio Guerreiro <ffsguerreiro@hotmail.com>
2020-07-13 12:11:44 +0100

    End Part II

--- ravenous/src/components/App/App.js
+++ ravenous/src/components/App/App.js
@@ -4,6 +4,21 @@ import './App.css';
 
 import SearchBar from '../SearchBar/SearchBar.jsx';
 import BusinessList from '../BusinessList/BusinessList.jsx';
+//import Business from '../Business/Business';
+
+let business = {
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+};
+
+const businesses = [business, business, business, business, business, business];
 
 export default class App extends Component {
   render() {
@@ -12,7 +27,7 @@ export default class App extends Component {
         <h1>ravenous</h1>
 
         <SearchBar />
-        <BusinessList />
+        <BusinessList businesses={businesses} />
       </div>
     );
   }
